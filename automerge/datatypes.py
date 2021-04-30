e9cd5a8a89309cef396ac28d9c0d1b0b75ad627e
Vedant Roy <vroy101@gmail.com>
2021-04-29 23:49:24 -0700

    override operators

--- automerge/datatypes.py
+++ automerge/datatypes.py
@@ -10,17 +10,67 @@ class Map(dict):
         super().__init__(iter_, **kwargs)
         if recent_ops is None:
             recent_ops = {}
-        # TODO: Do we need type tags?
+        # TODO: Do we need type tags? (probably not)
         self.type = "map"
         self.object_id = object_id
         self.recent_ops = recent_ops
+        self._frozen = False
+        mutable_methods = [
+            # TODO: Why can't we override operator methods using
+            # this approach?
+            # "__setitem__",
+            # "__delitem__",
+            "clear",
+            "pop",
+            "popitem",
+            "setdefault",
+            "update",
+        ]
+        override_mutable_methods(self, dict, mutable_methods)
 
     def get_pred(self, key):
         return list(self.recent_ops[key].keys()) if key in self.recent_ops else []
 
+    def __setitem__(self, key, value):
+        if self._frozen == True:
+            raise Exception(f"Cannot set item on {self} outside of change block")
+        return dict.__setitem__(self, key, value)
+
+    def __delitem__(self, key):
+        if self._frozen == True:
+            raise Exception(f"Cannot delete item on {self} outside of change block")
+        return dict.__delitem__(self, key)
+
+    def __deepcopy__(self, memo):
+        kvs = []
+        for k, v in self.items():
+            kvs.append(deepcopy((k, v)))
+        copy = Map(
+            kvs,
+            deepcopy(self.object_id),
+            deepcopy(self.recent_ops),
+        )
+        copy._frozen = self._frozen
+        return copy
+
+
+def override_mutable_methods(instance, super_class, names):
+    super_attrs = vars(super_class)
+    attrs = vars(instance)
+    for name in names:
+        old_method = attrs[name] if name in attrs else super_attrs[name]
+        wrapper = create_no_mutation_wrapper(old_method)
+        # https://stackoverflow.com/questions/1015307/python-bind-an-unbound-method
+        # https://docs.python.org/2/howto/descriptor.html
+
+        # This line "binds" (allows `wrapper` to accept `self`) `wrapper` to the current instance
+        bound = wrapper.__get__(instance)
+        attrs[name] = bound
+
 
 def create_no_mutation_wrapper(method):
     def wrapper(self, *args, **kwargs):
+        print("calling: " + method.__name__)
         if self._frozen == True:
             raise Exception(
                 f'Cannot call mutating method: "{method.__name__}" on {self} outside of change block'
@@ -46,30 +96,31 @@ class List(list):
         # Taken from here: https://www.w3schools.com/python/python_ref_list.asp
         # Might not need to override *every* mutable method since (maybe), for example, `append` is implemented in terms of `insert`
         # But I suspect not since Python devs probably want `list` to be as fast as possible, which means inlining code?
+        # (this comment also applies to the Map class)
         mutable_methods = [
+            # "__setitem__",
+            # "__delitem__",
             "append",
             "extend",
             "insert",
             "pop",
             "remove",
             "reverse",
-            "__setitem__",
-            "__delitem__",
         ]
-        super_attrs = vars(list)
-        attrs = vars(self)
-        for name in mutable_methods:
-            old_method = attrs[name] if name in attrs else super_attrs[name]
-            wrapper = create_no_mutation_wrapper(old_method)
-            # https://stackoverflow.com/questions/1015307/python-bind-an-unbound-method
-            # https://docs.python.org/2/howto/descriptor.html
-
-            # This line "binds" (allows `wrapper` to accept `self`) `wrapper` to the current instance
-            bound = wrapper.__get__(self)
-            attrs[name] = bound
+        override_mutable_methods(self, list, mutable_methods)
 
     def get_pred(self, idx):
         return list(self.recent_ops[idx].keys()) if idx < len(self.recent_ops) else []
+
+    def __setitem__(self, idx, value):
+        if self._frozen == True:
+            raise Exception(f"Cannot set item on {self} outside of change block")
+        return list.__setitem__(self, idx, value)
+
+    def __delitem__(self, key):
+        if self._frozen == True:
+            raise Exception(f"Cannot delete item on {self} outside of change block")
+        return list.__delitem__(self, key)
 
     def __deepcopy__(self, memo):
         storage = []
