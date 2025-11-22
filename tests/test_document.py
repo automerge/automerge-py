a57605593b2c2855e82bfe7896719c414684e3f2
Alex Good <alex@memoryandthought.me>
2025-11-22 15:02:03 +0000

    Implement text proxies

--- tests/test_document.py
+++ tests/test_document.py
@@ -1,38 +1,44 @@
 from automerge import Document
 
+
 def test_doc() -> None:
     doc = Document()
     with doc.change() as c:
-        c['hello'] = 'world'
+        c["hello"] = "world"
 
-    assert doc['hello'] == 'world'
-    
+    assert str(doc["hello"]) == "world"
+
+
 def test_doc_import_obj() -> None:
     doc = Document()
     with doc.change() as c:
-        c['hello'] = {'this': {'is': 'a', 'whole': 'tree'}, 'of': [{'obj': 'ect'}, 's']}
-        
-    assert doc.to_py() == {'hello': {'this': {'is': 'a', 'whole': 'tree'}, 'of': [{'obj': 'ect'}, 's']}}
-    
+        c["hello"] = {"this": {"is": "a", "whole": "tree"}, "of": [{"obj": "ect"}, "s"]}
+
+    assert doc.to_py() == {
+        "hello": {"this": {"is": "a", "whole": "tree"}, "of": [{"obj": "ect"}, "s"]}
+    }
+
+
 def test_nested_import() -> None:
     doc = Document()
     with doc.change() as c:
-        c['hello'] = {}
-        c['hello']['foo'] = 'bar'
-        
-    assert doc.to_py() == {'hello': {'foo': 'bar'}}
+        c["hello"] = {}
+        c["hello"]["foo"] = "bar"
+
+    assert doc.to_py() == {"hello": {"foo": "bar"}}
     {}.__getitem__
-    
+
+
 def test_iterable() -> None:
     doc = Document()
     with doc.change() as c:
-        c['list'] = [1, 2, 3]
-        c['map'] = {
-            'k': 'v',
-            'x': 'y',
+        c["list"] = [1, 2, 3]
+        c["map"] = {
+            "k": "v",
+            "x": "y",
         }
-    assert len(doc['list']) == 3
-    assert list(doc['list']) == [1, 2, 3]
-    assert len(doc['map']) == 2
-    assert list(doc['map']) == ['k', 'x']
-    assert list(doc['map'].items()) == [('k', 'v'), ('x', 'y')]
+    assert len(doc["list"]) == 3
+    assert list(doc["list"]) == [1, 2, 3]
+    assert len(doc["map"]) == 2
+    assert list(doc["map"]) == ["k", "x"]
+    assert list(doc["map"].items()) == [("k", "v"), ("x", "y")]
